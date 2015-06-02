4a0fa1d007f95cb55c55991e601bc8b6fe537139
Alex Jackson <jaj1014@gmail.com>
2015-06-02 14:26:14 -0400

    Update functions.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,64 +6,63 @@ The object will be generic referenced based on the [i] so that it can reuse the 
 
 function numberToNumeral() {
 
-	var number = document.getElementById("number").value;
+  var number = document.getElementById("number").value;
   
   if(number == "")
-	return document.getElementById("numeral").innerHTML = "Enter a Number";
+    return document.getElementById("numeral").innerHTML = "Enter a Number";
   
-	if(number > 3999 || isNaN(number))
-		return document.getElementById("numeral").innerHTML = "NA";
+  if(number > 3999 || isNaN(number))
+    return document.getElementById("numeral").innerHTML = "NA";
 		
-	if(number <= 0)	
+  if(number <= 0)	
     return document.getElementById("numeral").innerHTML = "NA";
     
 //Helper functions:
 //Converts number to array
-		function toArray(number) {
-			var array = [];
-			var	numberString = number.toString();
-			for(var i = 0; i < numberString.length; i++)
-				array.push(parseInt(numberString.charAt(i)));
-			return array;
-		}
+  function toArray(number) {
+    var array = [];
+    var	numberString = number.toString();
+    for(var i = 0; i < numberString.length; i++)
+      array.push(parseInt(numberString.charAt(i)));
+    return array;
+  }
 
-		//Iterate Numeral (for use when less than 3 or remainder of 5)
-		function iterateNumeral(value, numeral) {
-			var result = "";
-			for(var count = value; count > 0; count--)
-				result += numeral;	
-			return result;
-		}
+  //Iterate Numeral (for use when less than 3 or remainder of 5)
+  function iterateNumeral(value, numeral) {
+    var result = "";
+    for(var count = value; count > 0; count--)
+      result += numeral;	
+    return result;
+  }
 
-		//Reverse Array created with toArray()
-		var array = toArray(number).reverse();
+  //Reverse Array created with toArray()
+  var array = toArray(number).reverse();
 
-		//Array of Numerals
-		var numeralName = ["ones", "tens", "hundreds", "thousands"];
-		var numeralValue = {ones: ["I", "V", "X"], tens: ["X", "L", "C"], hundreds: ["C", "D", "M"], thousands: ["M"]};
-		//Roman Numeral Holder
-		var romanNumeral = [];
+  //Array of Numerals
+  var numeralName = ["ones", "tens", "hundreds", "thousands"];
+  var numeralValue = {ones: ["I", "V", "X"], tens: ["X", "L", "C"], hundreds: ["C", "D", "M"], thousands: ["M"]};
+  //Roman Numeral Holder
+  var romanNumeral = [];
 		
-		//Iterate over the reversed array of the original number
-		for(var i = 0; i < array.length; i++) {
-			//Set up variables to cycle through numeralName & numeralValue
-			var nameHolder = numeralName[i];
-			var arrayHolder = numeralValue[nameHolder];
+  //Iterate over the reversed array of the original number
+  for(var i = 0; i < array.length; i++) {
+    //Set up variables to cycle through numeralName & numeralValue
+    var nameHolder = numeralName[i];
+    var arrayHolder = numeralValue[nameHolder];
 
-			//Logic to generate the roman numeral
-			if(array[i] <= 3) {
-				romanNumeral.unshift(iterateNumeral(array[i], arrayHolder[0]));
-			} else if(array[i] <= 4) {
-				romanNumeral.unshift(arrayHolder[0] + arrayHolder[1]);
-			} else if(array[i] <= 5) {
-				romanNumeral.unshift(arrayHolder[1]);
-			} else if(array[i] <= 8 ) {
-				romanNumeral.unshift(arrayHolder[1]+ iterateNumeral(array[i] - 5, arrayHolder[0]));
-			} else { 
-				romanNumeral.unshift(arrayHolder[0] + arrayHolder[2]);
-			}
-		}
+    //Logic to generate the roman numeral
+    if(array[i] <= 3) {
+      romanNumeral.unshift(iterateNumeral(array[i], arrayHolder[0]));
+    } else if(array[i] <= 4) {
+      romanNumeral.unshift(arrayHolder[0] + arrayHolder[1]);
+    } else if(array[i] <= 5) {
+      romanNumeral.unshift(arrayHolder[1]);
+    } else if(array[i] <= 8 ) {
+      romanNumeral.unshift(arrayHolder[1]+ iterateNumeral(array[i] - 5, arrayHolder[0]));
+    } else { 
+      romanNumeral.unshift(arrayHolder[0] + arrayHolder[2]);
+    }
+  }
 
-	return document.getElementById("numeral").innerHTML = romanNumeral.join("");	
-
+  return document.getElementById("numeral").innerHTML = romanNumeral.join("");	
 }
